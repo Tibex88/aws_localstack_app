2091b6dfe71a3097ec386035974b89376810082b
tibex88 <tibesolomon7@gmail.com>
2024-11-04 14:27:18 +0300

    add: tailwind

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,13 +7,13 @@ import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
+    <div className="bg-red-400">
     <Routes>
       <Route path="/" element={<h1>Home</h1>}/>
       <Route path="/about" element={<h1>About</h1>}/>
       <Route path="*" element={<h1>Page not found</h1>}/>
     </Routes>
-    </>
+    </div>
   )
 }
 
