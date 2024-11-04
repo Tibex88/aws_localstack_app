81dbb36f72a70f18761cf5a91a1cc2c85b2abc4e
tibex88 <tibesolomon7@gmail.com>
2024-11-04 18:42:01 +0300

    add: response utility

---
+++ packages/backend/src/libs/response.ts
@@ -0,0 +1,21 @@
+export const success = (body: any) => {
+    return buildResponse(200, body);
+  };
+  
+  export const failure = (body: any) => {
+    return buildResponse(500, body);
+  };
+  
+  export const not_found = (body: any) => {
+    return buildResponse(404, body);
+  };
+  
+  const buildResponse = (statusCode: number, body: any) => ({
+    statusCode: statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(body),
+  });
+  
